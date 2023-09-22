64e2384a41195587e046eef6c88e65632e5391c2
Andreu Puchades <andpucpas@alu.edu.gva.es>
2023-09-22 19:07:38 +0200

    first commit

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,31 +1,41 @@
 'use strict'
 
 function letters(cadena) {
-
+	return cadena.length;
 }
 
 function words(cadena) {
-
+	return cadena.split(" ").length;
 }
 
 function upperString(cadena) {
-
+	return cadena.toUpperCase();
 }
 
 function titleString(cadena) {
+	const arrayPalabras = cadena.split(" ");
 
+	const capitalizedWords = arrayPalabras.map(arrayPalabras => {
+	  if (arrayPalabras.length > 0) {
+		return arrayPalabras.charAt(0).toUpperCase() + arrayPalabras.slice(1).toLowerCase();
+	  } else {
+		return arrayPalabras;
+	  }
+	});
+    
+	return capitalizedWords.join(" ");
 }
 
 function backwardsLetters(cadena) {
-
+	return cadena.split("").reverse().join("");
 }
 
 function backwardsWords(cadena) {
-
+	return cadena.split(" ").reverse().join(" ");
 }
 
 function palindrome(cadena) {
-
+	return cadena.replaceAll(" ", "").split("").reverse().join("").toLowerCase() === cadena.replaceAll(" ", "").toLowerCase();
 }
 
 module.exports = {
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,21 @@
 'use strict'
 
+const {
+    letters,
+    words,
+    upperString,
+    titleString,
+    backwardsLetters,
+    backwardsWords,
+    palindrome
+} = require("./functions");
 
+const frase = prompt("Introduzca una frase");
+
+console.log(letters(frase));
+console.log(words(frase));
+console.log(upperString(frase));
+console.log(titleString(frase));
+console.log(backwardsLetters(frase));
+console.log(backwardsWords(frase));
+console.log(palindrome(frase));
